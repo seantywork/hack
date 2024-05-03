410908862022578aba5fa0b8d2088b253d54c26a
seantywork <seantywork@gmail.com>
2024-05-03 18:48:01 +0900

    move cc

--- ros2/ros_ws/README.md
+++
@@ -1,5 +0,0 @@
-# hardware interface
-
-colcon build --symlink-install
-
-ros2 launch ros2hwif main.launch.py
--- ros2/ros_ws/src/hardwareinterface/setcanv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-
-sudo modprobe vcan
-sudo ip link add dev vcan0 type vcan
-sudo ip link set up vcan0
-

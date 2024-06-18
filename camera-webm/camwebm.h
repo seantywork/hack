80f68fda048b82a369beb27a44533743bd6ba2ba
seantywork <seantywork@gmail.com>
2024-06-18 17:26:10 +0900

    camera to webm

---
+++ camera-webm/camwebm.h
@@ -0,0 +1,18 @@
+#ifndef _CAM_WEBM_
+#define _CAM_WEBM_
+
+
+
+#include <libavcodec/avcodec.h>
+#include <libavdevice/avdevice.h>
+#include <libavfilter/avfilter.h>
+#include <libavformat/avformat.h>
+#include <libavutil/avutil.h>
+#include <libavfilter/buffersink.h>
+#include <libavfilter/buffersrc.h>
+
+
+
+
+
+#endif

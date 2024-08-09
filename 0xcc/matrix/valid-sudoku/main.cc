d12080ab0f6cd77a33c281df49a7e639f6feeb0e
seantywork <seantywork@gmail.com>
2024-08-09 11:15:45 +0900

    modifying matrix

--- 0xcc/matrix/valid-sudoku/main.cc
+++ 0xcc/matrix/valid-sudoku/main.cc
@@ -5,6 +5,102 @@
 using namespace std;
 
 
+class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+
+        for(int i = 0 ; i < 9; i++){
+
+            vector<char> x;
+            vector<char> y;
+
+
+            for(int j = 0 ; j < 9; j ++){
+
+
+                if((i % 3 == 0) && (j % 3 == 0)){
+
+                    vector<char> sub;
+
+                    for(int si = i; si < si + 3; si++){
+
+                        for(int sj = j; sj < sj + 3; sj++){
+
+                            char subc = board[si][sj];
+
+                            if(subc == '.'){
+                                continue;
+                            }
+
+                            
+
+                            printf("%c \n", subc);
+
+                            if(find(sub.begin(), sub.end(), subc) != sub.end()){
+                                printf("find?\n");
+                                return false;
+
+                            } else {
+                                
+                                sub.push_back(subc);
+                            }
+
+                        }
+
+
+                    }
+
+
+                    if(sub.size() == 0){
+                        return false;
+                    }
+                }
+
+                char xc = board[i][j];
+                char yc = board[j][i];
+
+                if(xc != '.'){
+                    if(find(x.begin(), x.end(), xc) != x.end()){
+
+                        return false;
+                    } else {
+                        x.push_back(xc);
+                    }
+                }
+
+                if(yc != '.'){
+
+                    if(find(y.begin(), y.end(), yc) != y.end()){
+
+                        return false;
+
+                    } else {
+
+                        y.push_back(yc);
+                    }
+
+
+                }
+
+            }
+
+            if(x.size() == 0){
+
+                return false;
+            }
+
+            if(y.size() == 0){
+
+                return false;
+            }
+
+                        
+        }
+
+        return true;
+    }
+};
+
 int main()
 {
 
@@ -12,7 +108,46 @@ int main()
 
     for(int t = 0 ; t < T; t ++){
 
+        vector<vector<char>> board;
+
+
+        for(int i = 0; i < 9; i++){
+
+            vector<char> row;
+
+            char* line_raw = readline();
+            
+            normalize_line(line_raw);
+
+            line_raw = ltrim(rtrim(line_raw));
+
+            int ellen = 0;
+            
+            char** line = split_string2(line_raw, &ellen);
+
+            for(int i = 0; i < ellen; i++){
+
+                char* c = line[i];
+
+                row.push_back(*c);
+
+            }
+
+            board.push_back(row);
+
+        }
+
+
+        Solution s;
         
+        bool result = s.isValidSudoku(board);
+
+        if(result){
+
+            printf("true\n");
+        } else {
+            printf("false\n");
+        }
     }
 
     return 0;

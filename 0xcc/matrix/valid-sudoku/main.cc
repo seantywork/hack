eb7ae67bb0917c3749f373ea9cc93653fecdebb4
seantywork <seantywork@gmail.com>
2024-08-09 09:42:53 +0900

    matrix cretion

---
+++ 0xcc/matrix/valid-sudoku/main.cc
@@ -0,0 +1,19 @@
+#include "common.h"
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+
+    int T = parse_int(readline());
+
+    for(int t = 0 ; t < T; t ++){
+
+        
+    }
+
+    return 0;
+}

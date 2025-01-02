87562272a31bccdf42f49d9213416d4d7182d365
seantywork <seantywork@gmail.com>
2025-01-02 17:23:41 +0900

    add channel

---
+++ channel/main.c
@@ -0,0 +1,32 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include "chan.h"
+
+chan_t* chan;
+
+void* ping()
+{
+
+    sleep(1);
+    // Send blocks until receiver is ready.
+    chan_send(chan, "ping");
+    return NULL;
+}
+
+int main()
+{
+    // Initialize unbuffered channel.
+    chan = chan_init(0);
+
+    pthread_t th;
+    pthread_create(&th, NULL, ping, NULL);
+
+    // Receive blocks until sender is ready.
+    void* msg;
+    chan_recv(chan, &msg);
+    printf("%s\n", (char *)msg);
+
+    // Clean up channel.
+    chan_dispose(chan);
+}
